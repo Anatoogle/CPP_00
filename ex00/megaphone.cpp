a4f8b825b9772bb69021ade5c39fddf89ec08d06
asemykin <asemykin@vbox.myguest.virtualbox.org>
2025-11-10 10:05:02 +0100

    added space in ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: asemykin <asemykin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 11:16:39 by asemykin          #+#    #+#             */
-/*   Updated: 2025/10/15 11:35:34 by asemykin         ###   ########.fr       */
+/*   Updated: 2025/11/10 09:55:18 by asemykin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int main (int argc, char **argv)
 	{
 		for(int i = 1; argv[i]; i++)
 		{
+			if(i > 1)
+				std::cout << " ";
 			for(int j = 0; argv[i][j]; j++)
 				std::cout << (char)std::toupper(argv[i][j]);
 		}
